e96c906e4893afc3c9507da98785eac7ec9dac51
Matt Bengston <bengsfort@gmail.com>
2023-12-12 06:54:59 +0200

    scaffold part2solver

--- 2023/day5/sources/part2solver.cpp
+++ 2023/day5/sources/part2solver.cpp
@@ -1,4 +1,7 @@
 #include "part2solver.h"
+#include "parsing.h"
+#include "debug.h"
+
 #include <string>
 
 // TODO: impl
@@ -16,20 +19,59 @@ namespace day5
 {
 using namespace day5;
 
-part_2_solver::part_2_solver() : solver()
-{
-	// todo
-};
-
+part_2_solver::part_2_solver() : solver() {};
 
 void part_2_solver::parse_line(int line_number, std::string *curr_line)
 {
-	// todo
+	if (line_number == 1) {
+		return this->init_seeds(curr_line);
+	}
+
+	if (curr_line->empty()) {
+		return;
+	}
+
+	if (curr_line->rfind(':') != std::string::npos) {
+		return this->init_map(curr_line);
+	}
+
+	this->_test_maps.push_back(parse_map_line(curr_line));
 }
 
 long long part_2_solver::get_result()
 {
+	// get hardware thread limit
+	// split each individual range up by that limit
+	// create threads for each -> thread_solve_range
+	// results should be in _curr_range_pending
+	// get smallest of those, toss into _pending_results (should this just be _pending result?)
+	// Keep going until all ranges have been handled
+	// profit
+
     return 0;
+}
+
+void part_2_solver::init_seeds(std::string *curr_line)
+{
+	parse_seed_ids(curr_line, &this->_seed_ranges);
+	auto& seed_ids = this->_seed_ranges;
+
+	std::cout << "parsed seeds" << std::endl;
+	print_ids(&seed_ids);
+}
+
+// these maybe arent needed as much now XD
+void part_2_solver::init_map(std::string *curr_line)
+{
+	auto map_name = parse_map_title(curr_line);
+	std::cout << " parsed " << map_name << std::endl;
+}
+
+
+void part_2_solver::thread_solve_range(long long start, long long len)
+{
+	// do normal pt1 flow, but use _test_maps instead of lines
+	// get the smallest 'result' from this range, put in the _pending_results vec
 }
 
 }
--- 2023/day5/sources/part2solver.h
+++ 2023/day5/sources/part2solver.h
@@ -2,6 +2,8 @@
 #define DAY5_PART2SOLVER_H
 
 #include <string>
+#include <vector>
+#include <mutex>
 #include "solver.h"
 
 namespace day5
@@ -9,12 +11,23 @@ namespace day5
 
 class part_2_solver : public day5::solver<long long>
 {
+
 public:
 	part_2_solver();
 
 	void parse_line(int line_number, std::string* curr_line) override;
 	long long get_result() override;
 
+private:
+	long long _result = LONG_LONG_MAX;
+	std::vector<std::array<long long, 3>> _test_maps;
+	std::mutex<std::vector<long long>> _pending_results{};
+	std::mutex<std::vector<long long>> _curr_range_pending{};
+	std::vector<long long> _seed_ranges{};
+
+	void init_seeds(std::string* curr_line);
+	void init_map(std::string* curr_line);
+	void thread_solve_range(long long start, long long len);
 };
 
 }
