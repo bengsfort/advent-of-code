26bfa334a8a6706f398305ab6944e560c738ee1b
Matt Bengston <bengsfort@gmail.com>
2024-12-03 23:54:49 +0200

    Refactor solution logic

--- 2024/two/solution/solution.go
+++ 2024/two/solution/solution.go
@@ -10,13 +10,42 @@ import (
 
 type Solution struct{}
 
+type Comparison struct {
+	difference int
+	direction  int
+}
+
 const max_threshold int = 3
 const min_threshold int = 1
 
-func compareNumbers(a, b int) (dir, diff int) {
-	diff = a - b
-	dir = bmath.IntSign(diff)
-	return dir, diff * dir
+func compareNumbers(a, b int) Comparison {
+	diff := a - b
+	dir := bmath.IntSign(diff)
+	return Comparison{
+		difference: diff,
+		direction:  dir,
+	}
+}
+
+func isPairValid(current int, previous int, direction int, ignoreDirection bool) (valid bool, comparison Comparison) {
+	if current == previous {
+		fmt.Println("Same numbers!", current, "->", previous)
+		return false, comparison
+	}
+
+	comparison = compareNumbers(current, previous)
+
+	if !ignoreDirection && direction != comparison.direction {
+		fmt.Println("Invalid direction change!", direction, "->", comparison.direction)
+		return false, comparison
+	}
+
+	if comparison.difference > max_threshold || comparison.difference < min_threshold {
+		fmt.Println("Exceeded thresholds! %d -> %d (diff: %d)", current, "->", previous, " (diff: ", comparison.difference, ")")
+		return false, comparison
+	}
+
+	return true, comparison
 }
 
 func isReportValid(values []string, dampen bool) (bool, error) {
@@ -26,6 +55,7 @@ func isReportValid(values []string, dampen bool) (bool, error) {
 
 	for i, value := range values {
 		if value == "" {
+			fmt.Println("\tempty string!")
 			continue
 		}
 
@@ -36,51 +66,30 @@ func isReportValid(values []string, dampen bool) (bool, error) {
 
 		if i == 0 {
 			previous = num
+			fmt.Println("\tvalid pair (frist)!")
 			continue
 		}
 
-		if num == previous {
-			fmt.Printf("Invalid number! %d == %d\n", previous, num)
+		valid, comparison := isPairValid(num, previous, direction, i == 1)
 
-			if dampen && !dampenerActive {
-				fmt.Printf("Dampener activated ")
-				dampenerActive = true
-				continue
-			}
-
-			return false, nil
+		if valid {
+			previous = num
+			direction = comparison.direction
+			fmt.Println("\tvalid pair!")
+			continue
 		}
 
-		dir, diff := compareNumbers(previous, num)
-
-		if i == 1 {
-			direction = dir
-		} else if direction != dir {
-			if dampen && !dampenerActive {
-				fmt.Printf("Dampener activated ")
-				dampenerActive = true
-				continue
-			}
-
-			fmt.Printf("Invalid direction change! %d -> %d\n", direction, dir)
-			return false, nil
+		if dampen && !dampenerActive {
+			fmt.Println("\tdampener active!")
+			dampenerActive = true
+			continue
 		}
 
-		if diff > max_threshold || diff < min_threshold {
-			if dampen && !dampenerActive {
-				fmt.Printf("Dampener activated ")
-				dampenerActive = true
-				continue
-			}
-
-			fmt.Printf("Invalid change! %d -> %d (diff %d)\n", previous, num, diff)
-			return false, nil
-		}
-
-		previous = num
+		fmt.Println("\tinvalid!\n")
+		return false, nil
 	}
 
-	fmt.Printf("valid!\n")
+	fmt.Println("\tvalid!\n")
 	return true, nil
 }
 
@@ -94,7 +103,7 @@ func (s *Solution) SolvePartOne(input []byte) (string, error) {
 		}
 
 		values := strings.Split(line, " ")
-		fmt.Printf("Validating report: %v ", values)
+		fmt.Println("Validating report:", values)
 		valid, err := isReportValid(values, false)
 		if err != nil {
 			return "", err
@@ -118,7 +127,7 @@ func (s *Solution) SolvePartTwo(input []byte) (string, error) {
 		}
 
 		values := strings.Split(line, " ")
-		fmt.Printf("Validating report: %v ", values)
+		fmt.Println("Validating report:", values)
 		valid, err := isReportValid(values, true)
 		if err != nil {
 			return "", err
